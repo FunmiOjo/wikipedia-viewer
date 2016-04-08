337235811379abdb731a3e6743f3030d35934f62
Funmi <oluwafunmi.ojo@gmail.com>
2016-04-08 16:40:43 -0400

    Update wiki_viewer.js

--- wiki_viewer.js
+++ wiki_viewer.js
@@ -138,10 +138,6 @@ function addAnchorsToTitles() {
 }
 
 
-//function changeFooterPosition() {
-	//document.getElementsByTagName("footer")[0].style.position = "relative";
-//}
-
 function stylePostSearchLayout() {
 	changeBackgroundColor();
 	changeFormMargin();
@@ -156,10 +152,8 @@ function addResultStructure() {
 	insertTitleElements();
 	insertPsInLis();
 	addAnchorsToTitles();
-	//if (document.getElementsByTagName("li").length >= 10) {
-		//changeFooterPosition();
-	//}
 }
+
 
 function tearDownResults() {
 	var ul = document.getElementsByTagName("ul")[0];
@@ -168,37 +162,9 @@ function tearDownResults() {
 	}
 }
 
-//working with data	
-//var searchResults = [{}, 
-	//{}, 
-	//{}, 
-	//{}, 
-	//{},
-	//{},
-	//{},
-	//{}, 
-	//{},
-	//{}];
-	
-//var urls = [["62: A Model Kit", "https://en.wikipedia.org/wiki/62:_A_Model_Kit"],
-		 //["Bestiario", "https://en.wikipedia.org/wiki/Bestiario"],
-		 //["Fantomas contra los vampiros multinacionales", "https://en.wikipedia.org/wiki/Fantomas_contra_los_vampiros_multinacionales"],
-		 //["Final del juego", "https://en.wikipedia.org/wiki/Final_del_juego"],
-		 //["Hopscotch (Julio Cort\u00e1zar novel)", "https://en.wikipedia.org/wiki/Hopscotch_(Julio_Cort%C3%A1zar_novel)"],
-		 //["Julio Cort\u00e1zar", "https://en.wikipedia.org/wiki/Julio_Cort%C3%A1zar"],
-		 //["Prosa del Observatorio", "https://en.wikipedia.org/wiki/Prosa_del_Observatorio"],
-		 //["The Winners (novel)", "https://en.wikipedia.org/wiki/The_Winners_(novel)"],
-		 //["Todos los fuegos el fuego", "https://en.wikipedia.org/wiki/Todos_los_fuegos_el_fuego"],
-		 //["\u00daltimo round", "https://en.wikipedia.org/wiki/%C3%9Altimo_round"]];
 
 
-//var urlResponse = 
-//{"batchcomplete":"","query":{"pages":
-	//{"13945880":{"pageid":13945880,"ns":0,"title":"62: A Model Kit","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":622266144,"length":879,"fullurl":"https://en.wikipedia.org/wiki/62:_A_Model_Kit","editurl":"https://en.wikipedia.org/w/index.php?title=62:_A_Model_Kit&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/62:_A_Model_Kit"},"14119524":{"pageid":14119524,"ns":0,"title":"Bestiario","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":696251568,"length":1609,"fullurl":"https://en.wikipedia.org/wiki/Bestiario","editurl":"https://en.wikipedia.org/w/index.php?title=Bestiario&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Bestiario"},"14063898":{"pageid":14063898,"ns":0,"title":"Fantomas contra los vampiros multinacionales","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":643907450,"length":1098,"fullurl":"https://en.wikipedia.org/wiki/Fantomas_contra_los_vampiros_multinacionales","editurl":"https://en.wikipedia.org/w/index.php?title=Fantomas_contra_los_vampiros_multinacionales&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Fantomas_contra_los_vampiros_multinacionales"},"14119796":{"pageid":14119796,"ns":0,"title":"Final del juego","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":712567889,"length":1969,"fullurl":"https://en.wikipedia.org/wiki/Final_del_juego","editurl":"https://en.wikipedia.org/w/index.php?title=Final_del_juego&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Final_del_juego"},"841100":{"pageid":841100,"ns":0,"title":"Hopscotch (Julio Cort\u00e1zar novel)","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":700005829,"length":21537,"fullurl":"https://en.wikipedia.org/wiki/Hopscotch_(Julio_Cort%C3%A1zar_novel)","editurl":"https://en.wikipedia.org/w/index.php?title=Hopscotch_(Julio_Cort%C3%A1zar_novel)&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Hopscotch_(Julio_Cort%C3%A1zar_novel)"},"334671":{"pageid":334671,"ns":0,"title":"Julio Cort\u00e1zar","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T17:28:29Z","lastrevid":712465349,"length":26469,"fullurl":"https://en.wikipedia.org/wiki/Julio_Cort%C3%A1zar","editurl":"https://en.wikipedia.org/w/index.php?title=Julio_Cort%C3%A1zar&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Julio_Cort%C3%A1zar"},"14064129":{"pageid":14064129,"ns":0,"title":"Prosa del Observatorio","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":693112986,"length":1239,"fullurl":"https://en.wikipedia.org/wiki/Prosa_del_Observatorio","editurl":"https://en.wikipedia.org/w/index.php?title=Prosa_del_Observatorio&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Prosa_del_Observatorio"},"14121254":{"pageid":14121254,"ns":0,"title":"The Winners (novel)","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":698374572,"length":1878,"fullurl":"https://en.wikipedia.org/wiki/The_Winners_(novel)","editurl":"https://en.wikipedia.org/w/index.php?title=The_Winners_(novel)&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/The_Winners_(novel)"},"14119906":{"pageid":14119906,"ns":0,"title":"Todos los fuegos el fuego","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":693112710,"length":1371,"fullurl":"https://en.wikipedia.org/wiki/Todos_los_fuegos_el_fuego","editurl":"https://en.wikipedia.org/w/index.php?title=Todos_los_fuegos_el_fuego&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/Todos_los_fuegos_el_fuego"},"13951844":{"pageid":13951844,"ns":0,"title":"\u00daltimo round","contentmodel":"wikitext","pagelanguage":"en","pagelanguagehtmlcode":"en","pagelanguagedir":"ltr","touched":"2016-03-30T16:50:09Z","lastrevid":693112919,"length":1114,"fullurl":"https://en.wikipedia.org/wiki/%C3%9Altimo_round","editurl":"https://en.wikipedia.org/w/index.php?title=%C3%9Altimo_round&action=edit","canonicalurl":"https://en.wikipedia.org/wiki/%C3%9Altimo_round"}}}};
-
-//var urlOre = urlResponse.query.pages;
-
-//extract titles from response	 
+//extract information from response	 
 function getTitles(searchResponse) {
 	var titles = [];
 	for (var i = 0; i < searchResponse.length; i++) {
@@ -209,11 +175,6 @@ function getTitles(searchResponse) {
 }
 
 
-function countDefinedTitles(titles) {
-	
-}
-
-//extract snippets from response
 function getSnippets(searchResponse) {
 	var snippets = [];
 	for (var i = 0; i < searchResponse.length; i++){
@@ -222,7 +183,7 @@ function getSnippets(searchResponse) {
 	return snippets;
 }
 
-//format titles to be used in AJAX request for urls
+
 function formatTitles(titles) {
 	var formattedTitles = "";
 	for (var i = 0; i < titles.length; i++) {
@@ -293,8 +254,8 @@ function getTitlesAndSnippetsIntoObj(searchInfo) {
 	searchResults = addSnippets(getSnippets(searchInfo), searchResults);
 }
 
-//filling in search results
 
+//filling in search results
 function populateTitles() {
 	var titleBlanks = document.getElementsByClassName("title-link");
 	for (var i = 0; i < titleBlanks.length; i++) {
